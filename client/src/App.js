177d47264d740e9dc075fe71f82a84863c873c27
jainpriyanshi <jainpriyanshi2407@gmail.com>
2019-11-21 23:07:37 +0545

    bugs fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Imageupload from './components/imageupload/Imageupload';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      <header>
+        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <Imageupload />
       </header>
