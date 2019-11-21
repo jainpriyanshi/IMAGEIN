1014ff60f3f388287e522ead3165c2cb650a45ae
jainpriyanshi <jainpriyanshi2407@gmail.com>
2019-11-21 23:34:21 +0545

    typist added

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Imageupload from './components/imageupload/Imageupload';
 import Header from './components/header/Header';
+import Typist from './components/typist/Typist';
 
 function App() {
   return (
     <div >
       <header>
         <Header />
+        <p> </p>
+        <Typist />
         <img src={logo} className="App-logo" alt="logo" />
         <Imageupload />
       </header>
---
+++ client/src/components/typist/Typist.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Typed from 'typed.js';
+
+export default class Typist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+    render() {
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <TypistComponent />
+            </div>
+        );
+    }
+}
+
+class TypistComponent extends Component {
+    componentDidMount() {
+        document.addEventListener('DOMContentLoaded', function () {
+            new Typed('.type', {
+                strings: ["inpaints", "blur", "resize", "crop"],
+                stringsElement: null,
+                typeSpeed: 80,
+                startDelay: 600,
+                backSpeed: 60,
+                backDelay: 1000,
+                loop: true,
+                loopCount: 5,
+
+                cursorChar: "|",
+                attr: null,
+                contentType: 'html',
+            });
+        });
+    }
+    render() {
+        return (
+            <div class="center">
+                <div class="typed">
+                    <span> IMAGICA </span><span class="type"></span><span> image </span>
+                </div>
+            </div>
+        )
+    }
+}
